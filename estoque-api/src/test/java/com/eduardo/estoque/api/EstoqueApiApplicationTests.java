8af419f981da4196e40ee3b21b163e8db09923b7
Eduardo <eduardo.gomes2@alunos.fho.edu.br>
2026-09-03 21:15:29 -0300

    adição inicial da estrutura spring boot

---
+++ estoque-api/src/test/java/com/eduardo/estoque/api/EstoqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.estoque.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
